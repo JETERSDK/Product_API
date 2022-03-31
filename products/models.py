1ddf1e35b405174de04bc4739ed655f9b2b5def5
Ennin Jeter <jeterennin@gmail.com>
2022-03-31 14:32:04 -0500

    Migrated models and updated files

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    descriptions = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventtory_quantity = IntegerField()
