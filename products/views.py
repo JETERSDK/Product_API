9ceae2ac7334b229ddab147b47b4a9ae118a7da1
Ennin Jeter <jeterennin@gmail.com>
2022-04-01 11:02:55 -0500

    Updated products file GET/POST

--- products/views.py
+++ products/views.py
@@ -1,14 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 from products import serializers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    products = Product.objects.all()
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
-    serializer = ProductSerializer(products, many=True)
-
-
-    return Response(serializer.data)
