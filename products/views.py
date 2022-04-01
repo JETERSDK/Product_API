aaff5b0afeb377be058dbd10c42d6b2f8326a398
Ennin Jeter <jeterennin@gmail.com>
2022-04-01 14:29:34 -0500

    Modified product/views file

--- products/views.py
+++ products/views.py
@@ -21,7 +21,7 @@ def products_list(request):
         
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
@@ -31,10 +31,9 @@ def product_detail(request, pk):
         serializer = ProductSerializer(product, data=request.data) 
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)  
-
-
-          
-    print(pk)
-
-
+        return Response(serializer.data) 
+    elif request.method == 'DELETE':
+         product.delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)    
+     
+   
